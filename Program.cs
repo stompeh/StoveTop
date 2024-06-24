f72d7a141d42ebd7fd27bc48292efbeac5bb6ccb
Stompy <daniel.br.falk@gmail.com>
2024-06-23 21:15:45 -0400

    First push

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Discord;
+using Discord.Interactions;
+using Discord.Utils;
+using Discord.Net;
+using Discord.Rest;
+using Discord.WebSocket;
+using System.Collections.Generic;
+using System.Threading.Channels;
+using Discord_HoneyPot;
+
+public class StoveTop()
+{
+    static void Main() => new StoveTop().MainAsync().GetAwaiter().GetResult();
+
+    List<SocketTextChannel> honeyChannels = new List<SocketTextChannel>();
+
+    public async Task MainAsync()
+    {
+        DiscordSocketClient client = new DiscordSocketClient();
+
+        client.Log += Log;
+        client.JoinedGuild += CreateHoneyChannel;
+        client.GuildAvailable += CheckHoneyExists;
+        client.MessageReceived += CheckHoneyMessage;
+
+        TokenParser tokenParser = new();
+        if (!tokenParser.TryParseTokenFile()) 
+        { 
+            Console.WriteLine("[!] Create or check token.json for a bot token.");
+            return; 
+        }
+
+        await client.LoginAsync(TokenType.Bot, tokenParser.tokenFile.token);
+        await client.StartAsync();
+        
+        await Task.Delay(-1);
+    }
+
+    async Task CheckHoneyExists(SocketGuild socketGuild)
+    {
+        bool honeyExists = false;
+        foreach (var channel in socketGuild.Channels)
+        {
+            if (channel.Name == "honeypot")
+            {
+                honeyChannels.Add(socketGuild.GetTextChannel(channel.Id));
+                Console.WriteLine("honeypot exists in guild {0}", socketGuild.Name);
+                honeyExists = true;
+            }
+        }
+    }
+
+    async Task CreateHoneyChannel(SocketGuild socketGuild)
+    {
+        var temp = await socketGuild.CreateTextChannelAsync("honeypot"); // TODO: Generate randomized or let user determine name
+        honeyChannels.Add(socketGuild.GetTextChannel(temp.Id));
+        Console.WriteLine("Created honeypot channel in {0}", socketGuild.Name);
+    }
+
+    async Task CheckHoneyMessage(SocketMessage socketMessage)
+    {
+        var channel = socketMessage.Channel as SocketTextChannel;
+        var author = socketMessage.Author as SocketGuildUser;
+        var guild = channel.Guild;
+
+        foreach (var textChannel in honeyChannels)
+        {
+            if (channel.Id == textChannel.Id)
+            {
+                await DeleteUserMessages(guild, author);
+                await guild.GetUser(author.Id).KickAsync("spammer");
+                Console.WriteLine("Kicked user {0}", socketMessage.Author.Username);
+
+            }
+        }
+    }
+
+    async Task DeleteUserMessages(SocketGuild socketGuild, SocketGuildUser socketUser)
+    {
+        foreach (var channel in socketGuild.Channels)
+        {
+            if (channel.GetChannelType() == ChannelType.Text)
+            {
+                var textChannel = channel as SocketTextChannel;
+                var messages = await textChannel.GetMessagesAsync(1).FlattenAsync();
+                var userMessages = messages.Where(m => m.Author.Id == socketUser.Id);
+                if (userMessages.Any())
+                {
+                    await textChannel.DeleteMessagesAsync(userMessages);
+                    Console.WriteLine("Removed messages from channel {0} in guild {1}", channel.Name, channel.Guild.Name);
+                }
+
+            }
+        }
+    }
+
+    private Task Log(LogMessage message)
+    {
+        Console.WriteLine(message.Message);
+        return Task.CompletedTask;
+    }
+}
---
+++ TokenParser.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace Discord_HoneyPot
+{
+    // Handles different methods of populating the Discord Bot token used for StoveTop.
+
+    public record TokenFile
+    {
+        public string token { get; set; }
+        public string server { get; set; }
+    }
+
+    internal class TokenParser
+    {
+        // Local file
+        public TokenFile tokenFile;
+
+        public bool TryParseTokenFile()
+        {
+            if (!File.Exists("token.json")) 
+            {
+                Console.WriteLine("[X] Could not find token.json");
+                return false; 
+            }
+
+            var tokenStuff = File.ReadAllText("token.json");
+            tokenFile = JsonSerializer.Deserialize<TokenFile>(tokenStuff);
+            return true;
+        }
+
+    }
+}
